d9525c2630161b77c784de1d5fb82ebb7905cf9b
Michelle David <rachaelholy@gmail.com>
2022-12-05 17:01:25 +0100

    Reset form after submit. Removed console logs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-   
+   const form = document.getElementById('new-blog')
    let blogArray = []
 
     function blogArry() {
@@ -17,33 +17,33 @@
     fetch("https://apis.scrimba.com/jsonplaceholder/posts")
        .then(res => res.json())
        .then(data => {
-          console.log(data, "before submit")
+         
         blogArray = data.slice(0, 5)
         blogArry()
        })
 
-document.getElementById('new-blog').addEventListener('submit', function(e){
-    e.preventDefault()
-    const title = document.getElementById('blog-title').value
-    const body = document.getElementById('blog-body').value
+       form.addEventListener('submit', function(e){
+            e.preventDefault()
+            const title = document.getElementById('blog-title').value
+            const body = document.getElementById('blog-body').value
 
-    const data = {
-        title: title,
-        body: body
-    }
+            const data = {
+                title: title,
+                body: body
+            }
 
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-.then(res => res.json())
-.then(data => {
-    console.log(data, 'after submit')
-    blogArray.unshift(data)
-    blogArry()
+            fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            blogArray.unshift(data)
+            blogArry()
+            form.reset()
 
 })
 })
