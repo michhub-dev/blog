12006437c223f9f0042a12e92e8f39e5adc3699a
Michelle David <rachaelholy@gmail.com>
2022-12-05 16:13:39 +0100

    Created a function that display the blog array

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
+
+   
+   const blogArray = []
+
+    function blogArry() {
+        let html = ''
+        for(let blog of blogArray){
+            html += `
+            <h3>${blog.title}</h3>
+            <p>${blog.body}</p><hr/>
+            `
+        }
+        
+        document.getElementById("articles").innHTML = html
+    }
+
 document.getElementById('new-blog').addEventListener('submit', function(e){
     e.preventDefault()
+    const title = document.getElementById('blog-title').value
+    const body = document.getElementById('blog-body').value
+
     fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
     method: 'POST',
     body: JSON.stringify({
-       title: 'new blog',
-       constent: false
+       title: title,
+       constent: body
     }),
     headers: {
         'content-type': 'application/json'
     }
 })
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+ 
+    blogArray.unshift(data)
+    blogArry()
+
+    // document.getElementById("articles").innHTML = data.slice(0,5)
+})
 })
